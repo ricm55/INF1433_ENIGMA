aa296a89c1b73384a2193afdfc73edc74b0794dd
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-12-03 20:03:46 -0500

    User can encrypt and decrypt a message only one time

--- services/crypto.py
+++ services/crypto.py
@@ -20,64 +20,79 @@ class Crypto:
     def encryption(self):
         self.mode = "ENCRYPT"
         self.determineTexteATraiter()
+        self.compteurLettres = 0
+        
+        #config l'interface
+        #Disable le output
+        
         self.etapeSuivante()
         print("mode encryption")
 
     def decryption(self):
         self.mode = "DECRYPT"
         self.determineTexteATraiter()
+        self.compteurLettres = len(self.texteATraiter) - 1
         self.etapeSuivante()
         print("mode decryption")
 
     def etapeSuivante(self):
-        
-        if self.mode == "ENCRYPT":
-            self.compteurLettres = 0
-        
-        elif self.mode == "DECRYPT":
-            self.compteurLettres = len(self.texteATraiter) - 1
-        else:
-            #L'utilisateur est dans aucun mode
-            return;
-        
+
+        #L'utilisateur n'a choisi aucun mode d'encryption
+        if self.mode == "NEUTRE":
+            return
+
+        self.NettoyerAffichage()
+
         lettreAEncrypt = self.texteATraiter[self.compteurLettres]
 
-        rotor_mode = 1
+        #Permet de savoir quel range du rotor colorier
+        range_rotor = 1
+
         #Obtenir le numero de la lettre et la colorier
-        numLettre = ord(lettreAEncrypt.upper())-65
-        self.colorierComposant(self.lettres.lettres_composants[numLettre],rotor_mode)
+        numCase = ord(lettreAEncrypt.upper())-65
+        self.colorierComposant(self.lettres.lettres_composants[numCase],range_rotor)
         
         composants = [self.rotor1,self.rotor2,self.rotor3]
+       
+        #Colorier la montee
+        for x in range(0,3):
+                valCalcule = composants[x].rotor[range_rotor][numCase]
+                self.colorierComposant(composants[x].edit[range_rotor][numCase],range_rotor)
+                numCase = (valCalcule + numCase) % 26
         
-        for x in range (0,2):
-            for x in range(0,3):
-                valCalcule = composants[x].rotor[rotor_mode][numLettre]
-                self.colorierComposant(composants[x].edit[1][numLettre],rotor_mode)
-                numLettre = valCalcule + numLettre
-            self.colorierComposant(self.reflect.ref_composants[numLettre],rotor_mode)
-            numLettre = valCalcule + self.reflect.reflecteur[numLettre]
-            rotor_mode = 0
-        """    
-        #Obtenir la valeur dans le rotor et colorier cette valeur
-        val_r1 = self.rotor1.rotor[1][numLettre]
-        self.rotor1.edit[1][numLettre].setStyleSheet("color:red;")
-        caseAColorier = val_r1 + numLettre
-
-        val_r2 = self.rotor2.rotor[1][caseAColorier]
-        self.rotor2.edit[1][caseAColorier].setStyleSheet("color:red;")
-        caseAColorier = val_r2 + caseAColorier
+        #Colorier le reflecteur
+        self.colorierComposant(self.reflect.ref_composants[numCase],range_rotor)
+        print(f"{numCase} + {self.reflect.reflecteur[numCase]}",end=' = ')
+        numCase = ( numCase + self.reflect.reflecteur[numCase] ) % 26
+        print(numCase)
+        #Changer la rangee des rotors
+        range_rotor = 0
         
-        val_r3 = self.rotor3.rotor[1][caseAColorier]
-        self.rotor3.edit[1][caseAColorier].setStyleSheet("color:red;")
-        caseAColorier = val_r3 + caseAColorier
-        """
-        """
-        val_ref = self.reflect.reflecteur[caseAColorier]
-        self.reflect.ref_composants.edit[1][caseAColorier].setStyleSheet("color:red;")
+        #Colorier la descente
+        for x in range(2,-1,-1):
+            valCalcule = composants[x].rotor[range_rotor][numCase]
+            self.colorierComposant(composants[x].edit[range_rotor][numCase],range_rotor)
+            numCase = valCalcule + numCase
         
-        caseAColorier = val_ref + caseAColorier
-        """
+        #Colorier la lettre final
+        self.colorierComposant(self.lettres.lettres_composants[numCase],range_rotor)
         
+        #Mettre la lettre final dans le output
+        
+        if self.mode == "ENCRYPT":
+            self.texteInput_Decryption.edit.insertPlainText(self.lettres.lettres[numCase])
+            self.compteurLettres +=1
+        
+        elif self.mode == "DECRYPT":
+            self.texteInput_Encryption.edit.insertPlainText(self.lettres.lettres[numCase])
+            self.compteurLettres -=1
+            print(f"compteur: {self.compteurLettres}")
+        
+        if self.compteurLettres == len(self.texteATraiter) or self.compteurLettres <0:
+            print("Fini l'encryption")
+            self.finaliserEncryption()
+            self.mode = "NEUTRE"
+            return
     def determineTexteATraiter(self):
         #Obtenir le input de l'utilisateur
         if self.mode == "ENCRYPT":
@@ -85,8 +100,8 @@ class Crypto:
         elif self.mode == "DECRYPT":
             self.texteATraiter = self.texteInput_Decryption.edit.toPlainText()
     
-    def colorierComposant(self,x,montee):
-        if montee:
+    def colorierComposant(self,x,range_rotor):
+        if range_rotor:
             x.setStyleSheet(
             "color:red;\
             background-color:#E0E0E0;"
@@ -96,6 +111,31 @@ class Crypto:
             "color:blue;\
             background-color:#E0E0E0;"
             )
+    def NettoyerAffichage(self):
+        #composants_a_nettoyer = [self.lettres.lettres_composants,self.rotor1,self.rotor2,self.rotor3,self.reflecteur.reflecteur]
+        #Nettoyer les lettres
+        for lettre in self.lettres.lettres_composants:
+            lettre.setStyleSheet("color:None;")
+
+        #Nettoyer les rotors
+        rotors = [self.rotor1,self.rotor2, self.rotor3]
+        for rotor in rotors:
+            for case in rotor.edit[0]:
+                case.setStyleSheet("color:None;")
+            for case in rotor.edit[1]:
+                case.setStyleSheet("color:None;")
+                
+        #Nettoyer le reflecteur
+        for case in self.reflect.ref_composants:
+            case.setStyleSheet("color:None;")
+        
+    def finaliserEncryption(self):
+        if self.mode == "ENCRYPT":
+            self.texteInput_Decryption.edit.setStyleSheet("border: 2px solid blue;")
+        else:
+            self.texteInput_Encryption.edit.setStyleSheet("border: 2px solid blue;")
+        
+        self.mode = "NEUTRE"
         
 
         
